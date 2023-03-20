55c4bfaa0c5ba0bf2bedd0deba4d5ad49d847bb4
Verjan <jeisonverjan@gmail.com>
2023-03-20 10:38:34 -0500

    Adding try/catch index

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 import {app} from './src/app.js'
 import './src/database/mongodb.js'
 
+try {
+    app.listen(3000)
+    console.log(`Server on port ${3000}`)
+} catch (error) {
+    console.log(error)
+}
 
-app.listen(3000)
-console.log(`Server on port ${3000}`)
